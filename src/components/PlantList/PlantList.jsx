a124be4a72a0ad95b5fb141315695865d9f691b3
Dustin Hagstrom <dustnhgstrm@gmail.com>
2024-01-24 20:57:28 -0600

    wrote the redux saga boiler plate code. wrote the logic to make a db call on load to get all of the plant data and to display it on the DOM using sagas.

--- src/components/PlantList/PlantList.jsx
+++ src/components/PlantList/PlantList.jsx
@@ -5,10 +5,19 @@ import { useDispatch, useSelector} from 'react-redux';
 function PlantList() {
     const dispatch = useDispatch();
 
+    // return entire store state
     const reduxState = useSelector(store => store);
+
+    const getPlants = () => {
+        // dispatch an action to the redux rootSaga
+        dispatch({
+            type: "FETCH_PLANTS"
+        })
+    }
 
     useEffect(() => {
         // dispatch an action to request the plantList from the API
+        getPlants();
     }, []); 
 
     return (
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,28 +1,52 @@
-import { createStore, combineReducers } from 'redux';
+import axios from "axios";
+import { createStore, combineReducers, applyMiddleware } from "redux";
+import logger from "redux-logger";
+import createSagaMiddleware from "redux-saga";
+import { takeLatest, put } from "redux-saga/effects";
 
 // this startingPlantArray should eventually be removed
 const startingPlantArray = [
-  { id: 1, name: 'Rose' },
-  { id: 2, name: 'Tulip' },
-  { id: 3, name: 'Oak' }
+    { id: 1, name: "Rose" },
+    { id: 2, name: "Tulip" },
+    { id: 3, name: "Oak" },
 ];
 
 const plantList = (state = startingPlantArray, action) => {
-  switch (action.type) {
-    case 'ADD_PLANT':
-      return [ ...state, action.payload ]
-    default:
-      return state;
-  }
+    switch (action.type) {
+        case "ADD_PLANT":
+            return [...state, action.payload];
+        default:
+            return state;
+    }
 };
 
-// 🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥
-// Note that the store is currently not
-// configured to utilize redux-saga OR
-// redux logger!
+function* fetchPlants() {
+    try {
+        const allPlantsRes = yield axios.get("/api/plants");
+
+        yield put({
+            type: "SET_PLANTS",
+            payload: allPlantsRes.data,
+        });
+    } catch (error) {
+        console.log("we got ourselves an error up in here.");
+        console.error(error);
+    }
+}
+
+function* rootSaga() {
+    // call generator functions to dispatch to reducers.
+
+    yield takeLatest("FETCH_PLANTS", fetchPlants);
+}
+
+const sagaMiddleware = createSagaMiddleware();
+
 const store = createStore(
-  combineReducers({ plantList }),
+    combineReducers({ plantList }),
+    applyMiddleware(sagaMiddleware, logger)
 );
-// 🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥
+
+sagaMiddleware.run(rootSaga);
 
 export default store;
